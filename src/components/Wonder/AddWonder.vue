925c22e5b4f99fd1cae28cb450b22386abb40b5c
Angkana016 laraverl10-week9 <myemail@example.com>
2024-02-13 14:56:33 +0700

    first commit

---
+++ src/components/Wonder/AddWonder.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container mt-4">
+        <div class="row">
+
+            <div class="col-md-5 mb-1 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header p-0">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bird_Diversity_2013.png/450px-Bird_Diversity_2013.png" class="card-img-top" alt="wonders">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header bg-primary">
+                        <span class="text-white fs-5">
+                            ลงทะเบียนนกที่สวยที่สุดในโลก
+                        </span>
+                    </div>
+                    <div class="card-body text-success">
+                        <form @submit.prevent="addWonder">
+                            <div class="mb-1">
+                                <label for="">ชื่อนกที่สวยที่สุดในโลก</label>
+                                <input type="text" class="form-control" v-model="wonderState.name" />
+                            </div>
+                            <div class="mb-1">
+                                <label for="">ตำแหน่ง ที่อยู่ นกที่สวยที่สุดในโลก</label>
+                                <input type="text" class="form-control" v-model="wonderState.label" />
+                            </div>
+                            <div class="mb-1">
+                                <label for="">ลิงค์ภาพนกที่สวยที่สุดในโลก</label>
+                                <input type="text" class="form-control" v-model="wonderState.imageUrl" />
+                            </div>
+
+                            <div class="mb-1">
+
+                                <button type="submit" class="btn btn-primary text-white shadow">
+                                    ลงทะเบียนนกที่สวยที่สุดในโลก
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const wonderState = reactive({
+    name: '',
+    location: '',
+    imageUrl:''
+})
+
+const router = useRouter()
+
+const addWonder = async () => {
+    await axios.post(`https://json-server-vue3-d109.onrender.com/wonders`,wonderState)
+        .then(() => {
+            console.log(wonderState)
+            router.push({name:'wonders'})
+        })
+    alert('ลงทะเบียนนกที่สวยที่สุดในโลก เรียบร้อยแล้ว')
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+.container {
+    font-family: 'Sarabun', sans-serif;
+
+}
+
+.form-label {
+    color: rgb(18, 229, 102);
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container my-5">
+    <div class="card shadow" style="width: 20rem;">
+      <div class="card-header p-0">
+        { <img src="https://ngthai.com/app/uploads/2017/07/1-2.jpg"
+            alt="img">},
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">About Page</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Read...</a>
+      </div>
+    </div>
+  </div>
+</template>
